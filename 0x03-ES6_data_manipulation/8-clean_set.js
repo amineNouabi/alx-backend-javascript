92b304ac1b40a617cb9c8c56d63000e9eaea6e8b
amineNouabi <aminenouabi9@gmail.com>
2024-10-07 18:28:45 +0100

    Ficed clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,11 +4,11 @@
  * @param {string} startString
  */
 export default function cleanSet(set, startString) {
-  if (!set || !startString || !(set instanceof Set) || !(startString instanceof String)) return '';
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') return '';
   let str = '';
 
   set.forEach((setElem) => {
-    if (setElem.startsWith(startString)) {
+    if (typeof setElem === 'string' && setElem.startsWith(startString)) {
       if (str.length > 0) str += '-';
       str += setElem.substring(startString.length);
     }
