acbdd2b6614431e8827b3c572dfd017f67d36397
amineNouabi <aminenouabi9@gmail.com>
2024-10-07 18:39:52 +0100

    Ficed clean set

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -6,11 +6,13 @@ export const weakMap = new WeakMap();
  */
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
+    weakMap.set(endpoint, 0);
     return;
   }
+
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+
   if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 }
