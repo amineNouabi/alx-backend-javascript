429dbba6ec96e3cf85744ed8b5d31ca52957514d
amineNouabi <aminenouabi9@gmail.com>
2024-11-25 19:04:16 +0100

    nodejs

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,32 +1,51 @@
 const http = require('http');
-const { readFileSync } = require('fs');
+const { readFile } = require('fs');
 
 const PORT_NUMBER = 1245;
-const FILE_PATH = process.argv[2];
+const FILE_PATH = process.argv.length > 2 ? process.argv[2] : '';
 
-const [, ...rows] = readFileSync(FILE_PATH).toString().trim().split('\n');
-const studentsByField = {};
-rows.forEach((row) => {
-  const [firstName, , , field] = row.split(',');
-  if (!Object.keys(studentsByField).includes(field)) {
-    studentsByField[field] = [firstName];
-    return;
-  }
-  studentsByField[field].push(firstName);
-});
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    readFile(filePath, (err, data) => {
+      if (err) { return reject(new Error('Cannot load the database')); }
+      const report = [];
+      const [, ...rows] = data.toString().trim().split('\n');
+
+      const studentsByField = {};
+      rows.forEach((row) => {
+        const [firstName, , , field] = row.split(',');
+        if (!Object.keys(studentsByField).includes(field)) {
+          studentsByField[field] = [firstName];
+          return;
+        }
+        studentsByField[field].push(firstName);
+      });
+
+      report.push(`Number of students: ${rows.length}`);
+      Object.keys(studentsByField).forEach((field) => {
+        report.push(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+      });
+      return resolve(report.join('\n'));
+    });
+  });
+}
 
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
 
-  if (req.url === '/') res.write('Hello Holberton School!');
+  if (req.url === '/') res.end('Hello Holberton School!');
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
-    res.write(`Number of students: ${rows.length}\n`);
-    Object.keys(studentsByField).forEach((field) => {
-      res.write(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}\n`);
-    });
+    countStudents(FILE_PATH)
+      .then((report) => {
+        res.write(report);
+      })
+      .catch((err) => {
+        res.write(err.message);
+      }).finally(() => {
+        res.end();
+      });
   }
-  return res.end();
 });
 
 app.listen(PORT_NUMBER || 1245);
